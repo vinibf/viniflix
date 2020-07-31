a921417f025b74b4cf9d99e2aa29f121de5ff859
Vinicius BF <vini91bf@gmail.com>
2020-07-31 00:27:12 -0300

    Initial commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from '../../assets/img/Logo.png';
+//import ButtonLink from './components/ButtonLink';
+import './Menu.css';
+import Button from '../Button';
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="ViniFlix logo" />
+      </a>
+
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
