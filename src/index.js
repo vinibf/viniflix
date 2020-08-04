30f08f17fe102afe8f53baae08911694ce8be73b
Vinicius BF <vini91bf@gmail.com>
2020-08-03 23:33:41 -0300

    Creating pages new video and register category

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import './reset.css';
+
+import Home from './pages/Home';
+import RegisterVideo from './pages/register/Video';
+import RegisterCategory from './pages/register/Category';
+
+// Desafio: definir o componente
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cadastro/video" component={RegisterVideo} />
+      <Route path="/cadastro/categoria" component={RegisterCategory} />
+      <Route component={Pagina404} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
